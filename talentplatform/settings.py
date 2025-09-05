1c29b146febc6673bea340c805b23d96ce03f3fa
Juliet Nakawesi <julietcristiana780@gmail.com>
2025-09-05 10:12:01 +0300

    talenthub

--- talentplatform/settings.py
+++ talentplatform/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-zgl!we9$%*#i9@yb2xs*b36_0a=2$iuv53gv8h2afs3nkkb*73
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['sports-talent.onrender.com', '127.0.0.1']
 
 
 # Application definition
